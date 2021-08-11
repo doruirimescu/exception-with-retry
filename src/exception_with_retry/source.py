f9d649e5f6ffd8a434735100990b9cb2149e9738
Doru Irimescu <doru.irimescu@sensible4.fi>
2021-08-11 16:26:18 +0300

    Add init commit

---
+++ src/exception_with_retry/source.py
@@ -0,0 +1,48 @@
+import time
+
+__author__ = "Doru Irimescu"
+__copyright__ = "Doru Irimescu"
+__license__ = "MIT"
+
+
+# ---- Python API ----
+# The functions defined in this section can be imported by users in their
+# Python scripts/interactive interpreter, e.g. via
+# `from exception_with_retry.source import ExceptionWithRetry`,
+# when using this Python module as a library.
+
+
+class ExceptionWithRetry:
+    """[summary]"""
+
+    def __init__(self, method, n_retry=5, sleep_time_s=0.5):
+        """Construct ExceptionWithRetry
+
+        Args:
+            method ([type]): Method which throws exception to call.
+            n_retry (int, optional): Number of retries. Defaults to 5.
+            sleep_time_s (float, optional): Sleep time between consecutive
+            retries in seconds. Defaults to 0.5.
+        """
+        self._method = method
+        self._retries = 0
+        self._max_retries = n_retry
+        self._sleep_time_s = sleep_time_s
+
+    def run(self, args):
+        """[summary]
+
+        Args:
+            args ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
+        if self._retries < self._max_retries:
+            self._retries = self._retries + 1
+            try:
+                return self._method(*args)
+            except Exception as e:
+                print(e)
+                time.sleep(self._sleep_time_s)
+                return self.run(args)
---
+++ tests/test_1.py
@@ -0,0 +1,69 @@
+import unittest
+
+from exception_with_retry.source import ExceptionWithRetry
+
+__author__ = "Doru Irimescu"
+__copyright__ = "Doru Irimescu"
+__license__ = "MIT"
+
+
+class TestMethods:
+    def method(self, arg):
+        arg.append(arg[-1] + 20)
+        raise Exception("Not gonna happen")
+
+
+def method(arg):
+    arg.append(arg[-1] + 1)
+    raise Exception("Not gonna happen")
+
+
+def method2(arg1, arg2):
+    arg1.append(arg1[-1] + arg2)
+    raise Exception("Not gonna happen")
+
+
+def method3():
+    return 25
+
+
+def method_caller(arg):
+    method(arg)
+
+
+class TestExceptionWithRetry(unittest.TestCase):
+    def test_1(self):
+        ewr = ExceptionWithRetry(method, 5, 0.0)
+        arg = [0]
+        ewr.run([arg])
+        self.assertEqual(arg, [0, 1, 2, 3, 4, 5])
+
+    def test_2(self):
+        ewr = ExceptionWithRetry(method, 0, 0.0)
+        arg = [0]
+        ewr.run([arg])
+        self.assertEqual(arg, [0])
+
+    def test_3(self):
+        ewr = ExceptionWithRetry(method2, 2, 0.0)
+        arg = [0]
+        ewr.run([arg, 1])
+        self.assertEqual(arg, [0, 1, 2])
+
+    def test_4(self):
+        ewr = ExceptionWithRetry(method_caller, 2, 0.0)
+        arg = [1, 2]
+        ewr.run([arg])
+        self.assertEqual(arg, [1, 2, 3, 4])
+
+    def test_5(self):
+        t = TestMethods()
+        ewr = ExceptionWithRetry(t.method, 5, 0.0)
+        arg = [0]
+        ewr.run([arg])
+        self.assertEqual(arg, [0, 20, 40, 60, 80, 100])
+
+    def test_6(self):
+        ewr = ExceptionWithRetry(method3, 5, 0.0)
+        result = ewr.run([])
+        self.assertEqual(result, 25)
